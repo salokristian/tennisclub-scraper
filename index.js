a9c95798d6cc5b58638dccb15530e210473e80b9
Kristian Salo <salo.kristian@hotmail.com>
2022-11-12 18:13:08 +0200

    Add support for switching clubs

--- index.js
+++ index.js
@@ -6,33 +6,38 @@ async function waitAndClick(page, selector) {
 }
 
 function getDataFromGroupText(groupText) {
-  const [, locationRaw, playersRaw] = groupText.split('\n');
-  const [, location] = locationRaw.match(/\s*(.*)varaa/);
+  const [, timeAndLocationRaw, playersRaw] = groupText.split('\n');
+  const [, timeAndLocation] = timeAndLocationRaw.match(/\s*(.*)varaa/);
   const [, players] = playersRaw.match(/\s*(.*)/);
-  return { location, players };
+  return { timeAndLocation, players };
 }
 
-async function main() {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+async function switchToClub(page, club) {
+  const clubToOptionValue = {
+    kirte: '3071031',
+    smash: '3068439',
+  };
 
-  await page.goto('https://tennisclub.fi');
+  await page.goto('https://tennisclub.fi/pelaaja/site/user-club');
 
-  const playerButtonSelector = 'a[href="/pelaaja"]';
-  await waitAndClick(page, playerButtonSelector);
+  const optionValue = clubToOptionValue[club];
+  await page.select('#selectclubform-person_id', optionValue);
 
-  await page.waitForSelector('#loginformfront-username');
-  await page.type('#loginformfront-username', process.env.USERNAME, {
-    delay: 50,
-  });
-  await page.type('#loginformfront-password', process.env.PASSWORD, {
-    delay: 50,
-  });
-  await page.click('.btn-success');
+  await page.click('.btn-primary');
 
   await page.waitForNetworkIdle({ idleTime: 2000 });
+}
 
+async function getAvailableGroups(page) {
+  // After switching clubs we need to load to group view twice, and visit some other page in
+  // between. The first load always only displays 'Korvausjärjestelmä ei ole käytössä
+  // valmennustasoillasi.'
   await page.goto('https://tennisclub.fi/pelaaja/kj-store/search');
+  await page.waitForNetworkIdle({ idleTime: 500 });
+  await page.goto('https://tennisclub.fi/pelaaja/group/index');
+  await page.waitForNetworkIdle({ idleTime: 500 });
+  await page.goto('https://tennisclub.fi/pelaaja/kj-store/search');
+  await page.waitForNetworkIdle({ idleTime: 500 });
 
   const availableWeeks = await page.evaluate(() =>
     Array.from(
@@ -58,7 +63,37 @@ async function main() {
     availableGroups.push(...weekAvailableGroups);
   }
 
-  console.log(availableGroups);
+  return availableGroups;
+}
+
+async function main() {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto('https://tennisclub.fi');
+
+  const playerButtonSelector = 'a[href="/pelaaja"]';
+  await waitAndClick(page, playerButtonSelector);
+
+  await page.waitForSelector('#loginformfront-username');
+  await page.type('#loginformfront-username', process.env.USERNAME, {
+    delay: 50,
+  });
+  await page.type('#loginformfront-password', process.env.PASSWORD, {
+    delay: 50,
+  });
+  await page.click('.btn-success');
+
+  await page.waitForNetworkIdle({ idleTime: 2000 });
+
+  await switchToClub(page, 'kirte');
+  const kirteGroups = await getAvailableGroups(page);
+
+  await switchToClub(page, 'smash');
+  const smashGroups = await getAvailableGroups(page);
+
+  const allGroups = [...kirteGroups, ...smashGroups];
+  console.log(allGroups);
 
   await browser.close();
 }
